bc74c2b0999d5c6ac29e2b8f74529ae58d0352a2
murremway <76540706+murremway@users.noreply.github.com>
2021-12-11 20:47:32 -0500

    image id updated

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@
     sudo curl -L https://github.com/docker/compose/releases/download/1.29.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
     sudo chmod +x /usr/local/bin/docker-compose
     docker pull murremway/yinka-app
-    docker run -itd -p 3001:80 7da0f90273e1
+    docker run -itd -p 3001:80 439af64db489
     echo " "
     echo "Validating the versions Installed"
     echo "`docker -v` - is installed Successfully"
